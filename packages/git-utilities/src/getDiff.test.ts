0a7b970b53d0ce79eaa67d49cf8b772e3f0c1ef0
james <jameslnewell>
2022-06-28 23:45:12 +1000

    fix typechecks

--- packages/git-utilities/src/getDiff.test.ts
+++ packages/git-utilities/src/getDiff.test.ts
@@ -6,26 +6,25 @@ jest.mock("./exec");
 const execMock = exec as unknown as jest.Mock<ReturnType<typeof exec>>;
 
 describe("getDiff()", () => {
-  const directory = ".";
 
   test("returns an empty map when an empty string is returned", async () => {
     execMock.mockResolvedValue({ stdout: "", stderr: "" });
-    expect(await getDiff(directory)).toEqual({});
+    expect(await getDiff()).toEqual({});
   });
 
   test("returns an empty map when whitespace is returned", async () => {
     execMock.mockResolvedValue({ stdout: " ", stderr: "" });
-    expect(await getDiff(directory)).toEqual({});
+    expect(await getDiff()).toEqual({});
   });
 
   test("returns an empty map when newlines are returned", async () => {
     execMock.mockResolvedValue({ stdout: "\n", stderr: "" });
-    expect(await getDiff(directory)).toEqual({});
+    expect(await getDiff()).toEqual({});
   });
 
   test("returns a map when a single file has changed", async () => {
     execMock.mockResolvedValue({ stdout: "M package.json", stderr: "" });
-    expect(await getDiff(directory)).toEqual({
+    expect(await getDiff()).toEqual({
       "package.json": "M",
     });
   });
@@ -35,7 +34,7 @@ describe("getDiff()", () => {
       stdout: "M package.json\nA README.md",
       stderr: "",
     });
-    expect(await getDiff(directory)).toEqual({
+    expect(await getDiff()).toEqual({
       "README.md": "A",
       "package.json": "M",
     });
